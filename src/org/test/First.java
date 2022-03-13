1d96d82dd22be8bc13a2fcc84ca5ec48a7120bb1
SaranyaSwara <h.saranya99@gmail.com>
2022-03-13 20:43:50 +0530

    First code file added

---
+++ src/org/test/First.java
@@ -0,0 +1,15 @@
+package org.test;
+
+public class First {
+	private void empname() {
+		System.out.println("Saranya!");
+
+	}
+	static public void main(String args[]) {
+		System.out.println("Hello ");
+		First a=new First();
+		a.empname();
+	}
+			
+	}
+
